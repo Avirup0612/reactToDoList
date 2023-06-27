0086476d845018b43bb9c724e5785bea5042139a
Avirup0612 <mitraavirup1@gmail.com>
2023-06-27 21:36:41 +0530

    updated to do list

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,55 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import TaskList from './TaskList';
 
-function App() {
+const App = () => {
+
+  const getData = () =>{
+    const data = localStorage.getItem('lists')
+    if(data){
+      return (JSON.parse(data));
+    }else {
+      return ([]);
+    }
+  }
+
   const [input, setInput] = useState("");
-  const[task, setTask] = useState([]);
+  const[task, setTask] = useState(getData());
+  const[edit, setEdit] = useState(false)
+  const [info, setInfo]= useState(null)
 
   const handleChange = (event) =>{
     setInput(event.target.value)
   }
 
+  useEffect(()=>{
+    localStorage.setItem("lists",JSON.stringify(task));
+  },[task])
+
   const submitTask = () =>{
-    if(input!=""){
-      setTask((prev)=>{
-        return([...prev,input])
-      });
+    if(input){
+      const allInputData = { id: new Date().getTime().toString(), name:input}
+      setTask([...task,[allInputData.id,allInputData.name]])
       setInput("");
     }
   }
 
-  const deleteTask = (id) =>{
+  const editTask = (currentElem) =>{
+    setEdit(true);
+    setInput(currentElem[1])
+    setInfo(currentElem);
+  }
+
+  const newSubmitTask = () =>{
+    info[1]=input;
+    setEdit(false);
+    setInput("");
+  }
+
+  const deleteTask = (serialno) =>{
     setTask((prev)=>{
-      return(prev.filter((elem, index) => {
-        return (index!==id)
+      return(prev.filter((elem) => {
+        return (elem[0]!==serialno)
       }))
     })
   }
@@ -36,12 +63,14 @@ function App() {
           </div>
           <div className='inputdiv'>
             <input className='taskinput' type='text' placeholder='Enter your tasks' onChange={handleChange} maxLength={60} value={input}/>
-            <button className='submitbtn' type='submit' onClick={submitTask}>+</button>
+            <button className={edit ? "nothing" : 'submitbtn' } type='submit' onClick={submitTask}><ion-icon name="add-outline"></ion-icon></button>
+            <button className={edit ? 'editsubmitbtn' : 'nothing' } type='submit' onClick={newSubmitTask}><ion-icon name="checkmark-outline"></ion-icon></button>
           </div>
+          <button className='removebtn' type='submit' onClick={()=>{setTask([])}}>Remove all</button>
           <div className='lists'>
             <ol>
             {task.map((val, index)=>{
-              return(<TaskList content={val} key={index} id={index} onSelect={deleteTask}/>)
+              return(<TaskList content={val[1]} key={index} editstatus={edit} onDelete={()=>{deleteTask(val[0])}} onEdit={()=>{editTask(val)}}/>)
             })}
             </ol>
           </div>
--- src/TaskList.jsx
+++ src/TaskList.jsx
@@ -2,10 +2,12 @@ import './App.css'
 
 
 const TaskList = (props) => {
+
     return(
         <>
             <div className="listitem">
-                <ion-icon name="close-outline" onClick={() =>{props.onSelect(props.id)}}></ion-icon>
+                <ion-icon name="close-outline" onClick={() =>{props.onDelete()}}></ion-icon>
+                <ion-icon name="create-outline" onClick={() =>{props.onEdit()}}></ion-icon>
                 <li>{props.content}</li>
             </div>
         </>
